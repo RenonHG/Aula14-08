becd52937b1db45ecd8875e4cd98d22633515520
RenonHG <141833668+RenonHG@users.noreply.github.com>
2023-08-14 20:13:14 -0300

    Add files via upload

---
+++ Ex3 Conversão de Temperatura/Ex3 Conversão de Temperatura/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_Conversão_de_Temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float C, F;
+
+            Console.Write("Insira a temperatura em ºC: ");
+            C = float.Parse(Console.ReadLine());
+
+            F = (C * 9 / 5) + 32;
+            Console.Write("A temperatura de " + C + " ºC, é equivalente a: " + F + " ºF\n");
+
+            Console.ReadKey();
+        }
+    }
+}
