93403aea0937deb7ca8a942b2015cc722db6cf36
RenonHG <141833668+RenonHG@users.noreply.github.com>
2023-08-14 20:05:38 -0300

    Add files via upload

---
+++ Ex1 Media de 3 Números/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2, n3;
+            
+            float media;
+            
+
+            Console.Write("Insira o 1º valor: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Insira o 2º valor: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Insira o 3º valor: ");
+            n3 = float.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3) / 3;
+
+            Console.Write("A média desses valores é: " + media + "\n");
+
+            
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Ex2 Troco de Compra/Ex2 Troco de Compra/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_Troco_de_Compra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float preco;
+            float pago;
+            float troco;
+
+            Console.Write("Preço do produto: R$");
+            preco = float.Parse(Console.ReadLine());
+            //Console.WriteLine("O preço do produto é: R$" + preco);
+
+            Console.Write("Valor pago pelo cliente: R$");
+            pago = float.Parse(Console.ReadLine());
+
+            if (pago < preco)
+            {
+                Console.WriteLine("Desculpe, infelizmente você não possui o valor necessário para comprar esse produto");
+            }
+            else 
+            {
+                troco = pago - preco;
+                Console.WriteLine("Troco: R$" + troco);
+            }
+            
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
